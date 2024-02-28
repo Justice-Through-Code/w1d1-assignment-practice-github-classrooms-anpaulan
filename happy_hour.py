e5f54cd6b4413ad6ec92438c307d1c02ad83bb6e
anpaulan <paulan21095@gmail.com>
2024-02-28 16:26:08 -0500

    v1

--- happy_hour.py
+++ happy_hour.py
@@ -14,13 +14,36 @@ def is_happy_hour(user_date, user_time):
         return 17 <= user_time.hour < 19  # Happy hour between 5 PM and 7 PM
 
 def is_christmas(date):
-    #ENTER CODE HERE
+    m = date.month
+    d = date.day
+    if m == 12 and d == 25:
+        return True
+    else: return False
+
+
 
 def is_easter(date):
     return date == calc_easter_sunday(date.year)
 
 def is_sunday(date):
+    # return date.weekday() == 6
     #ENTER CODE HERE
+    
+    year = date.year
+    a = year % 19
+    b = year // 100
+    c = year % 100
+    d = b // 4
+    e = b % 4
+    f = (b + 8) // 25
+    g = (b - f + 1) // 3
+    h = (19 * a + b - d - g + 15) % 30
+    i = c // 4
+    k = c % 4
+    l = (32 + 2 * e + 2 * i - h - k) % 7
+    if l == 1:
+        return True 
+    else: False
 
 def calc_easter_sunday(year):
     a = year % 19
@@ -38,9 +61,12 @@ def calc_easter_sunday(year):
     month = (h + l - 7 * m + 114) // 31
     day = ((h + l - 7 * m + 114) % 31) + 1
     return datetime.date(year, month, day)
+
+    
 if __name__ == "__main__":
     user_date = input("Enter date (YYYY-MM-DD): ")
     user_time = input("Enter time (HH:MM): ")
 
     result = is_happy_hour(user_date, user_time)
     print("Is it happy hour?", result)
+
